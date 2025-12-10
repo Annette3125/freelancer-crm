d58a16775bb4b35b5659f8e0611d78424d688821
Annette3125 <anetuks@gmail.com>
2025-12-10 22:04:40 +0200

    create admin for Client

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Client
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "status", "country", "created_at")
+    list_filter = ("status", "country")
+    search_fields = ("name", "email", "company")
