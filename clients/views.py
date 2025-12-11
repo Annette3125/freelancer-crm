43a853fa17f7de7b217d3cf8f4fbe2c3a88bb40d
Annette3125 <anetuks@gmail.com>
2025-12-12 00:27:01 +0200

    create FBV (functional view) for client_list

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Client
 
 # Create your views here.
+
+def client_list(request):
+    clients = Client.objects.all().order_by("name")
+    return render(request, "clients/client_list.html", {"clients": clients})
+
