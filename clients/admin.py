ae12456f83dcc10896a8f0877a8a7c86e2309f46
Annette3125 <anetuks@gmail.com>
2025-12-11 23:03:12 +0200

    update admin by adding Project Admin to panel. add migrations

--- clients/admin.py
+++ clients/admin.py
@@ -2,10 +2,17 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Client
+from .models import Client, Project
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ("name", "email", "status", "country", "created_at")
     list_filter = ("status", "country")
     search_fields = ("name", "email", "company")
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "client", "status", "budget", "start_date", "due_date")
+    list_filter = ("status", "client")
+    search_fields = ("title", "client__name")
