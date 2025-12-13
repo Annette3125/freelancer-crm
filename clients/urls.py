d01fadcd97e0e37d6cc09942517b2872d5662cd6
Annette3125 <anetuks@gmail.com>
2025-12-13 23:27:34 +0200

    add projects url for project_list

--- clients/urls.py
+++ clients/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.client_list, name="client_list"),
-    path("<int:pk>/", views.client_detail, name="client_detail")
-
+    path("<int:pk>/", views.client_detail, name="client_detail"),
+    path("projects/", views.project_list, name="project_list"),
 ]
 
