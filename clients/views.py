e5b5fc52c0e3c27e9b4644f090424f8638b02f16
Annette3125 <anetuks@gmail.com>
2025-12-13 22:01:54 +0200

    create view for client_detail

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Client
 
 # Create your views here.
@@ -7,3 +7,12 @@ def client_list(request):
     clients = Client.objects.all().order_by("name")
     return render(request, "clients/client_list.html", {"clients": clients})
 
+
+def client_detail(request, pk):
+    client = get_object_or_404(Client, pk=pk)
+    projects = client.projects.all().order_by("-created_at")
+    context = {
+        "client": client,
+        "projects": projects,
+    }
+    return render(request, "clients/client_detail.html", context)
