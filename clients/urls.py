ad1a0359391ad9d4ec1c61f69322e114fa271835
Annette3125 <anetuks@gmail.com>
2025-12-16 13:58:48 +0200

    update url for creating new client

--- clients/urls.py
+++ clients/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.client_list, name="client_list"),
     path("<int:pk>/", views.client_detail, name="client_detail"),
     path("projects/", views.project_list, name="project_list"),
+    path("new/", views.client_create, name="client_create")
 ]
 
