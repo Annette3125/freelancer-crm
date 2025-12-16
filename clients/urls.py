f7edf0596c744d3916b9b02dc6b82ff0aa0d9835
Annette3125 <anetuks@gmail.com>
2025-12-17 01:01:45 +0200

    add url path to project form to url.py and add url for button to new project in project_list.html.py

--- clients/urls.py
+++ clients/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.client_list, name="client_list"),
     path("<int:pk>/", views.client_detail, name="client_detail"),
     path("projects/", views.project_list, name="project_list"),
-    path("new/", views.client_create, name="client_create")
+    path("new/", views.client_create, name="client_create"),
+    path("projects/new", views.project_create, name="project_create"),
 ]
 
