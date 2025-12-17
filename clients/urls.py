0b92e9f88aa902ac7c3da15b6d3e3516b63bde71
Annette3125 <anetuks@gmail.com>
2025-12-18 01:21:26 +0200

    add url to project create for client

--- clients/urls.py
+++ clients/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("projects/", views.project_list, name="project_list"),
     path("new/", views.client_create, name="client_create"),
     path("projects/new", views.project_create, name="project_create"),
+    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),  # 👈 naujas
+
 ]
 
