a80832916b6f071cfe6ee05ffdd69fbdfcede899
Annette3125 <anetuks@gmail.com>
2025-12-18 21:23:45 +0200

    add new path for edit project

--- clients/urls.py
+++ clients/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path("", views.client_list, name="client_list"),
     path("<int:pk>/", views.client_detail, name="client_detail"),
     path("<int:pk>/edit/", views.client_update, name="client_update"),
-    path("projects/", views.project_list, name="project_list"),
     path("new/", views.client_create, name="client_create"),
-    path("projects/new", views.project_create, name="project_create"),
-    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),
-    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),
 
+    path("projects/", views.project_list, name="project_list"),
+    path("projects/new/", views.project_create, name="project_create"),
+    path("projects/<int:pk>/edit/", views.project_update, name="project_update"),
+
+    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),
 ]
-
