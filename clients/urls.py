8e8151bfc72a52501bc4829611eaff356adba46b
Annette3125 <anetuks@gmail.com>
2025-12-18 19:37:27 +0200

    create path for edit (client_update function) for client

--- clients/urls.py
+++ clients/urls.py
@@ -4,10 +4,12 @@ from . import views
 urlpatterns = [
     path("", views.client_list, name="client_list"),
     path("<int:pk>/", views.client_detail, name="client_detail"),
+    path("<int:pk>/edit/", views.client_update, name="client_update"),
     path("projects/", views.project_list, name="project_list"),
     path("new/", views.client_create, name="client_create"),
     path("projects/new", views.project_create, name="project_create"),
-    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),  # 👈 naujas
+    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),
+    path("<int:client_pk>/projects/new/", views.project_create_for_client, name="project_create_for_client"),
 
 ]
 
