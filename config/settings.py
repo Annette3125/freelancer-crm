a2b1f79201b36cbb1677db4c9e77dd10d2ac2068
Annette3125 <anetuks@gmail.com>
2025-12-19 19:02:46 +0200

    update settings.py, add .env.sample file

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
